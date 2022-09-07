8b14a61a6a52a0c760d097db2bb8eb4bbb2a6276
Viet Dang <vietdanghoang1705@gmail.com>
2022-09-07 15:18:33 +0700

    Final commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCO6wUR6A0dvUoszbUsD7i52yZVnjvZKgs",
+    authDomain: "simple-firebase-auth-5cc3f.firebaseapp.com",
+    projectId: "simple-firebase-auth-5cc3f",
+    storageBucket: "simple-firebase-auth-5cc3f.appspot.com",
+    messagingSenderId: "1024924273221",
+    appId: "1:1024924273221:web:d89940a09aceddbc8f312c"
+  };
+
+  export default firebaseConfig;
